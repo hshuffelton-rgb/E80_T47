df3dbecd3bc41d47a712c75090f157806bf3d5c5
drobles-stack <drobles@g.hmc.edu>
2026-03-26 13:45:20 -0700

    update lab

--- libraries/main/SurfaceControl.cpp
+++ libraries/main/SurfaceControl.cpp
@@ -54,15 +54,18 @@ void SurfaceControl::navigate(xy_state_t * state, gps_state_t * gps_state_p, int
 
     ///////////////////////////////////////////////////////////
     // INSERT P CONTROL CODE HERE
-    yaw_des = atan2(y_des - y, x_des - x);
-    yaw_error = angleDiff(yaw_des, yaw);
+    yaw_des = atan2(y_des - state->y, x_des - state->x);
+    yaw_error = angleDiff(yaw_des);
     u = Kp * yaw_error;
     uR = avgPower + u;
     uL = avgPower - u;
     uR = uR * Kr;
     uL = uL * Kl;
-    0 < uR < 127;
-    0 < uL < 127;
+    float lower_limit = 0;
+    float upper_limit = 127;
+
+    uR = std::clamp(uR, lower_limit, upper_limit);
+    uL = std::clamp(uL, lower_limit, upper_limit);
     ///////////////////////////////////////////////////////////
     
   }
